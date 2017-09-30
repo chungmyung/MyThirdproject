5c93dcb7bbf98f27161b930888fa5ec54f3fce43
JSKim <mkcjsk@gmail.com>
2017-09-30 17:20:22 +0900

    현재위치 정보 갖고 와 뿌리기  ( 권한체크 )

--- Map/src/main/java/com/chungmyung/map/MapsActivity.java
+++ Map/src/main/java/com/chungmyung/map/MapsActivity.java
@@ -1,18 +1,29 @@
 package com.chungmyung.map;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    public static final int REQUEST_CODE_LOCATION_PERMISSION = 1000;
     private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +33,80 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-}
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+
+        getLastLocation();
+    }
+
+    private void getLastLocation() {
+
+        //권한이 없으면
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            // 권한 요청
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION},
+                    REQUEST_CODE_LOCATION_PERMISSION);
+
+            return;
+        }
+
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 17.0f));
+
+                            Toast.makeText(MapsActivity.this, location.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                // 승인됨.
+                getLastLocation();
+            } else {
+                // 승인 안됨.
+                Toast.makeText(this, "승인 안됨.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -38,25 +121,18 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
 
         // Add a marker in Sydney and move the camera
-        LatLng youngTong = new LatLng(37.260407,127.07674599999996);
-        mMap.addMarker(new MarkerOptions().position(youngTong).title("청명고등학교"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(youngTong));
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(youngTong, 17.0f));
-        CameraUpdateFactory.zoomTo(6.0f);
-
-        // Add a marker in Sydney and move the camera
-        LatLng chomackgol = new LatLng(37.3536153,126.91856459999997);
+        LatLng chomackgol = new LatLng(37.3536153, 126.91856459999997);
         mMap.addMarker(new MarkerOptions().position(chomackgol).title("초막골생태공원"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(chomackgol));
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(chomackgol, 15.0f));
-        CameraUpdateFactory.zoomTo(6.0f);
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(chomackgol, 15.0f));
+//        CameraUpdateFactory.zoomTo(6.0f);
     }
 }
